8fa0f5b6fee2557be7b8b867397eeee194c95dd3
Chandan Kumar <30540406+cksudhir124@users.noreply.github.com>
2018-09-27 08:26:39 +0530

    add comment

--- Comp_Solution.java
+++ Comp_Solution.java
@@ -9,18 +9,15 @@ import java.util.Vector;
 
 public class School_Details
 {
-
     public static void main(String[] args) throws Exception
     {
         JFrame f=new JFrame("Details of Student");
         JButton b,b1;
-        
         b=new JButton("ADD");
         b1=new JButton("SHOW");
         b.setBounds(100,50,100,30);
         b1.setBounds(100,100,100,30);
-        
-        
+    // b button for enter a new frame to fill the all required details of student.    
         b.addActionListener(new ActionListener()
         {  
                public void actionPerformed(ActionEvent e)
@@ -56,6 +53,7 @@ public class School_Details
                    t5.setBounds(100,340,200,30);
                    jb.setBounds(200,390,100,30);
                    
+          //jb button for insert the filled student of details into database.         
                    jb.addActionListener(new ActionListener()
                    {  
                         public void actionPerformed(ActionEvent e)
@@ -109,6 +107,7 @@ public class School_Details
                    
                }
         });
+     // b1 button for showing the requied JTable of student details,which will be fetch from database table.   
         b1.addActionListener(new ActionListener()
         {  
                public void actionPerformed(ActionEvent e)
